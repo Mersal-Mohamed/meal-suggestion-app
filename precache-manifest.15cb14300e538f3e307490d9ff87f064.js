4ae5718d76ad8f13eeee5f6e5d3bd3a951b1d722
Mersal Mohamed <mersalmohamed20@gmail.com>
2019-12-12 14:58:15 +0200

    meal suggestion first commit

---
+++ precache-manifest.15cb14300e538f3e307490d9ff87f064.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c30ba20ab1cf92d42ec08162ac2b38a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e7691a5cc35d1d8f70e",
+    "url": "/static/css/main.d1a2b475.chunk.css"
+  },
+  {
+    "revision": "302485519c655f165d8f",
+    "url": "/static/js/2.03bf18e4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.03bf18e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e7691a5cc35d1d8f70e",
+    "url": "/static/js/main.d99c5f4e.chunk.js"
+  },
+  {
+    "revision": "69d3cc172a78f7b56820",
+    "url": "/static/js/runtime-main.e5371c5f.js"
+  }
+]);
